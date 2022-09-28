9ddfc109a62f755df1af2cc5ac040fac4aacc624
Shingo-Kamei <guijingxinwu352@gmail.com>
2022-09-28 21:23:38 +0900

    詳細画面

--- resources/views/appoints/edit.blade.php
+++ resources/views/appoints/edit.blade.php
@@ -16,52 +16,56 @@
 <div class="main">
     <h1 class="edit_title">Techis.Salon</h1>
 
-    <img class="edit_picture" src="{{ asset('img/fukuoka-riyou-gstyle1-860x645.jpg') }}" alt="">
+    <div class="edit-picture-wrrper">
+        <img class="edit_picture" src="{{ asset('img/fukuoka-riyou-gstyle1-860x645.jpg') }}" alt="">
+    </div>
 
 
     <div class="salon_data">
-        <label class="edit-name" for="edit-name">サロン名</label>
-        <span class="salon_name">Techis.Salon[テックアイエス.サロン]</span>
-
-        <div>
-            <label class="edit-time" for="edit-time">営業時間</label>
-            <span class="salon_time">9:00~22:00</span>
+        <div class="salon_name">
+            <label class="edit-name" for="edit-name">サロン名</label>
+            <span>Techis.Salon[テックアイエス.サロン]</span>
         </div>
 
-        <div>
-            <label class="edit-holiday" for="edit-holiday">定休日</label>
-            <span class="salon_holiday">第3月曜日</span>
-        </div>
+            <div class="salon_time">
+                <label class="edit-time" for="edit-time">営業時間</label>
+                <span>9:00~22:00</span>
+            </div>
 
-        <div>
-            <label class="edit-price" for="edit-price">価格</label>
-            <span class="salon_price">¥3,000~</span>
-        </div>
+            <div class="salon_holiday">
+                <label class="edit-holiday" for="edit-holiday">定休日</label>
+                <span>第3月曜日</span>
+            </div>
 
-        <div>
-            <label class="edit-address" for="edit-address">住所</label>
-            <span class="salon_address">IT県テック市アイエス町555-5</span>
-        </div>
+            <div class="salon_price">
+                <label class="edit-price" for="edit-price">価格</label>
+                <span>¥3,000~</span>
+            </div>
 
-        <div>
-            <label class="edit-tel" for="edit-tel">電話番号</label>
-            <span class="salon_tel">55555555555</span>
-        </div>
+            <div class="salon_address">
+                <label class="edit-address" for="edit-address">住所</label>
+                <span>IT県テック市アイエス町555-5</span>
+            </div>
 
-        <div>
-            <label class="edit-seat" for="edit-seat">座席数</label>
-            <span class="salon_seat">10席</span>
-        </div>
+            <div class="salon_tel">
+                <label class="edit-tel" for="edit-tel">電話番号</label>
+                <span>55555555555</span>
+            </div>
 
-        <div>
-            <label class="edit-crew" for="edit-crew">スタッフ人数</label>
-            <span class="salon_crew">5人</span>
-        </div>
+            <div class="salon_seat">
+                <label class="edit-seat" for="edit-seat">座席数</label>
+                <span>10席</span>
+            </div>
 
-        <div>
-            <label class="edit-parking" for="edit-parking">駐車場</label>
-            <span class="salon_parking">あり</span>
-        </div>
+            <div class="salon_crew">
+                <label class="edit-crew" for="edit-crew">スタッフ人数</label>
+                <span>5人</span>
+            </div>
+
+            <div class="salon_parking">
+                <label class="edit-parking" for="edit-parking">駐車場</label>
+                <span>あり</span>
+            </div>
     </div>
 </div>
 
