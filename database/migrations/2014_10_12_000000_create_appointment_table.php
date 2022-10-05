d5a3e38dfa4082388b8375786e2ba161dc961897
CeMoReOn <secorpention2020@gmail.com>
2022-10-05 21:53:55 +0900

    migrate更新

--- database/migrations/2014_10_12_000000_create_appointment_table.php
+++ database/migrations/2014_10_12_000000_create_appointment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAppointmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('appointments');
     }
 }
