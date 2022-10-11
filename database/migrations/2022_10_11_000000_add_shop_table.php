3d20e42d7cb44e8da94f589b0212d0ca3bb82ea3
CeMoReOn <secorpention2020@gmail.com>
2022-10-11 19:18:41 +0900

    migrate修正

---
+++ database/migrations/2022_10_11_000000_add_shop_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shops', function (Blueprint $table) {
+            $table->date('appoint_at')->after('parking')->comment('予約日');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
