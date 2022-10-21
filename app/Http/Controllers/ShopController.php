0901f060b68d4e0008bbfe02fee7e617c4fd5dbe
noripan13ten <nemoto@github>
2022-10-21 21:30:15 +0900

    修正

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $shops = Shop::get();
+        return view('shop/index')-with('shops',$shops);
+
+    }
+
+    public function create()
+    {
+        return view('shop/create');
+    }
+
+    public function edit($id)
+    {
+        $shop = Shop::find($id);
+        return view('shop/edit')->with('shop',$shop);
+    }
+
+    public function store(Request $request)
+    {
+        $shop = new Shop();
+        $shop->shop_name = $request->shop_name;
+        $shop->address = $request->shop_address;
+        $shop->open_hour = $request->shop_open_hour;
+        $shop->
+    }
+
+    //
+}
